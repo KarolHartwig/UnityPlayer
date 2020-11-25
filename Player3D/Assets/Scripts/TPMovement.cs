61193b98d881a38d2e13f7b01de5e289393d882d
KarolHartwig <59097463+KarolHartwig@users.noreply.github.com>
2020-11-25 17:11:39 +0100

    TPP
    Added third person perspective camera, cinemachine module and movement for player.

---
+++ Player3D/Assets/Scripts/TPMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPMovement : MonoBehaviour
+{
+    private float speed = 5f;
+    private float turnSmoothTime = .1f;
+    private float turnSmoothVelocity;
+
+    public CharacterController controller;
+    public Transform cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(x, 0, z).normalized;
+
+        if(direction.magnitude >= .1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
